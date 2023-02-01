c7cc4c3df1790b520dd3a4e4fa24d1c7daf686a2
Wiola Janczura <wiola.janczura@wp.pl>
2023-02-01 23:59:34 +0000

    almost

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react'
+import { Feedback } from './Feedback/Feedback';
+
+class App extends Component {
+    render() {
+      return (
+        <div>
+            <Feedback step={1} />
+        </div>
+      );
+
+    }
+}
+
